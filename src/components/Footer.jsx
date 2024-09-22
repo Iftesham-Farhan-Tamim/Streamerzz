d7a0a8e5bf4d5f181288aef23217fe752f7d9769
Farhan Tamim <farhantamim@Farhans-MacBook-Pro.local>
2024-09-22 14:03:21 +0600

    second commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,13 +5,13 @@ function Footer() {
     return (
         <footer className='mt-20 text-neutral-400'>
             <div className='max-w-7xl mx-auto px-4 border-t border-x-neutral-800'>
-                <div className='grid grid-cols-1 md:grid-cols-4 gap-8 mt-20'>
+                <div className='grid grid-cols-1 md:grid-cols-4 gap-8 mt-20 text-center sm:text-left'>
                     {FOOTER_CONTENT.sections.map((section, index) => (
                         <div key={index}>
-                            <h3 className='text-white font-medium mb-4 cursor-pointer'>
+                            <h3 className='text-white font-medium mb-6 cursor-pointer'>
                                 {section.title}
                             </h3>
-                            <ul className='space-y-2'>
+                            <ul className='space-y-2 mb-4'>
                                 {section.links.map((link, index) => (
                                     <li key={index}>
                                         <a className='cursor-pointer hover:text-neutral-200' href={link.url}>{link.text}</a>
